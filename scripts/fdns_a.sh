e3257afba6be8600f35194d5895d5c304bef0480
Jonathan Cran <jcran@intrigue.io>
2019-06-21 15:22:57 -0700

    updated fdns

--- scripts/fdns_a.sh
+++ scripts/fdns_a.sh
@@ -1,5 +1,5 @@
 # fetch the fdns_a file
-wget --quiet -O fdns_a.gz https://opendata.rapid7.com/sonar.fdns_v2/2019-01-25-1548417890-fdns_a.json.gz
+wget --quiet -O fdns_a.gz https://opendata.rapid7.com/sonar.fdns_v2/2019-05-26-1558831458-fdns_a.json.gz
 
 # extract and format our data
 gunzip --quiet -c fdns_a.gz | jq -r '.value + ","+ .name' | tr '[:upper:]' '[:lower:]' | rev > fdns_a.rev.lowercase.txt
